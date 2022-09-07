d4400a384c53a19b3ca3b980761670cbad5272db
Sugita <blackbox631@gmail.com>
2022-09-07 14:56:28 +0000

    7.1.3 ログイン状態の永続的保持終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,11 @@ class User < ApplicationRecord
   # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_dijest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # トークンがダイジェストと一致すればtrueを返します。
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end  
 end
