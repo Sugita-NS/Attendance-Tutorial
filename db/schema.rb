06f98c14d51bc6dfb95927c00f5ea3e380e5b29c
Sugita <blackbox631@gmail.com>
2022-10-10 07:08:07 +0000

    10.7 出勤日数を表示しよう終了

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20221003150550) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-09-29 23:00:00"
-    t.datetime "work_time", default: "2022-09-29 22:30:00"
+    t.datetime "basic_time", default: "2022-10-09 23:00:00"
+    t.datetime "work_time", default: "2022-10-09 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
